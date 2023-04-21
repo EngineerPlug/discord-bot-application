5c1e1e2bad8b462028b9dacab01038cb0800abb2
Martin Clark <webdeveloperplug@gmail.com>
2023-04-20 21:15:17 -0500

    Bot discord update

---
+++ src/index.js
@@ -0,0 +1,30 @@
+// Importing Discord js packages
+const { EmbedBuilder } = require('@discordjs/builders');
+const { GuildMember } = require('discord.js');
+const youtube = 'AIzaSyBU3ME_jTpdWHne0hakZiVgY4qxQQ53mbk';
+const youtubeUrl = 'https://www.googleapis.com/youtube/v3';
+
+
+//dotenv properties
+require('dotenv').config();
+
+//discord bot application
+
+const { Client } = require('discord.js');
+const Bot = new Client();
+
+module.exports = {
+    name: "guildMemberAdd",
+    execute(member) {
+        const { user, guild } = member;
+        const welcomeChannel = member.guild.channels.cache.get('933847397359026258');
+        const welcomeMessage = `Welcome <@${member.id}> to the guild fam!`;
+        welcomeChannel.send({content: welcomeMessage});
+    }
+}
+
+Bot.on('ready', () => {
+    console.log(`${Bot.user.tag} is successfully logged in!`);
+})
+
+Bot.login(process.env.Bot);
